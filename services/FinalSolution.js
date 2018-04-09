7fccd7b105effdc5fb36dc32f37623328bc6fef2
Kristian Patlevic <patlevic.kristian@gmail.com>
2018-04-09 15:36:00 +0200

    It's the final countdown

--- services/FinalSolution.js
+++ services/FinalSolution.js
@@ -2,47 +2,6 @@ const fibonacci = require('fibonacci');
 const {generatePrimeNumbers} = require('./NumberHelper.js');
 const {subsetsLessThan} = require('./powerSet.js');
 
-/*
-*
-* Bugged after (6)
-*
-*
-
-function mySolution(prime, n) {
-  let i = 0;
-  let j = 0;
-  let counter = 0;
-
-  let pomocnePole = [];
-  const vysledok = [];
-
-  while (j < prime.length) {
-    if (counter === 0) {
-      pomocnePole.push(prime[i]);
-      counter += prime[i];
-    }
-    if (counter.toString() === n.toString()) {
-      vysledok.push(pomocnePole);
-      pomocnePole = [];
-      counter = 0;
-      i++;
-    }
-    if (counter.toString() > n.toString()) {
-      for (let x = 0; x < parseInt(n, 10) - 3; x++) {
-        pomocnePole.pop();
-        counter -= prime[j];
-      }
-      j++;
-    }
-    if (counter !== 0) {
-      pomocnePole.push(prime[j]);
-      counter += prime[j];
-    }
-  }
-
-  return vysledok;
-}*/
-
 function getSolution(n) {
   const i = n;
 
